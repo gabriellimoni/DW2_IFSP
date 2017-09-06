45571cf7f4de1cb3d55e3afc49de3ca128be05d7
Eraldo <eraldogoncalvesjr@gmail.com>
2017-09-06 13:56:08 -0300

    my first commit

---
+++ InventarioIFSP/InventarioIFSP/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventarioIFSP.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EnviaLogin()
+        {
+            return View();
+        }
+    }
+}
