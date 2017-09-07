1ab8e6bd7aca1f51c105d156ab0cc9e7a6a1b5d7
gabriellimoni <gabrielblimoni@gmail.com>
2017-09-07 11:54:25 -0300

    Usuario com read e write

---
+++ Inventario_IFSPPRC/InventarioIFSP/InventarioIFSP/Controllers/InventarioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventarioIFSP.Controllers
+{
+    public class InventarioController : Controller
+    {
+        // GET: Inventario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
---
+++ Inventario_IFSPPRC/InventarioIFSP/InventarioIFSP/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventario_IFSPPRC.Models
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Matricula { get; set; }
+        public string Telefone { get; set; }
+
+        public List<int> Funcoes { get; set; } // padronizar numeração das funções
+    }
+}
