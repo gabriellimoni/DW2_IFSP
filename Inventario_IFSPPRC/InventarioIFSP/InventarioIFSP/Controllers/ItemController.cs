e4a9350b504a2ea936f82f48081e482872faf45c
gabriellimoni <gabrielblimoni@gmail.com>
2017-09-08 17:49:49 -0300

    Modelos versão beta criados

---
+++ Inventario_IFSPPRC/InventarioIFSP/InventarioIFSP/Controllers/ItemController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventarioIFSP.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Item/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Item/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Item/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Item/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Item/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Item/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Inventario_IFSPPRC/InventarioIFSP/InventarioIFSP/Models/Usuario.cs
+++ Inventario_IFSPPRC/InventarioIFSP/InventarioIFSP/Models/Usuario.cs
@@ -12,7 +12,64 @@ namespace Inventario_IFSPPRC.Models
         public string CPF { get; set; }
         public string Matricula { get; set; }
         public string Telefone { get; set; }
-
         public List<int> Funcoes { get; set; } // padronizar numeração das funções
+
+        public Usuario(int ID, string Nome, string CPF, string Matricula, string Telefone, List<int> Funcoes)
+        {
+            this.ID = ID;
+            this.Nome = Nome;
+            this.CPF = CPF;
+            this.Matricula = Matricula;
+            this.Telefone = Telefone;
+            this.Funcoes = Funcoes;
+        }
+
+        public Usuario(string Nome, string CPF, string Matricula, string Telefone, List<int> Funcoes)
+        {
+            this.Nome = Nome;
+            this.CPF = CPF;
+            this.Matricula = Matricula;
+            this.Telefone = Telefone;
+            this.Funcoes = Funcoes;
+        }
+
+        public Usuario(int ID)
+        {
+            this.ID = ID;
+        }
+
+
+
+
+        public Boolean insert()
+        {
+            // comandos para inserir 
+
+
+            return false;
+        }
+
+        public Boolean update()
+        {
+            // comandos para atualizar 
+
+
+            return false;
+        }
+
+        public Boolean delete()
+        {
+            // comandos para deletar 
+
+
+            return false;
+        }
+
+        public Boolean autenticar()
+        {
+
+
+            return false;
+        }
     }
 }
