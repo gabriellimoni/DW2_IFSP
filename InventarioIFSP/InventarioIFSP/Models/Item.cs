ec907c66eee3cac2791dd7d8ea383c4aac554cb9
Eraldo <eraldogoncalvesjr@gmail.com>
2017-09-13 10:19:38 -0300

    Models

---
+++ InventarioIFSP/InventarioIFSP/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventarioIFSP.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public int IdLocation { get; set; }
+        public string Patrimony { get; set; }
+        public int Category { get; set; }
+        public int SubCategory { get; set; }
+        public int Status { get; set; }
+        public string Description { get; set; }
+    }
+}
